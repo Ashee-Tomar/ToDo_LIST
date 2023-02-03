9bb42c5251ce6629d0cd406bd054d877d7123f49
RAJ MANJREKAR <manjrekarraj9@gmail.com>
2023-02-03 13:58:33 +0530

    APP UPDATE

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,6 +1,6 @@
 from unicodedata import name
 from django.urls import path
-from .views import mark_task, user_logout, welcome_page, add_task, delete_task,user_login, user_signup, user_logout
+from .views import mark_task, user_logout, welcome_page, add_task, delete_task,user_login, user_signup, user_logout, search_task
 
 urlpatterns = [
     path('',welcome_page,name='home_page'),
@@ -8,6 +8,7 @@ urlpatterns = [
     path('add_task', add_task, name="add_task"),
     path('delete_task/<str:id>', delete_task, name='delete_task'),
     path('mark_task/<str:id>', mark_task, name='mark_task'),
+    path('search/', search_task, name='search_task'),
     path('login/', user_login, name='user_login'),
     path('signup/', user_signup, name='user_signup'),
     path('logout/', user_logout, name='user_logout')
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate,login, logout
+from django.views.decorators.cache import cache_control
+from django.contrib.auth.decorators import login_required
 
 from .models import Tasks
 from .form import TaskForm
 from .signup_form import SignupForm
 
 # Create your views here.
+@login_required(login_url='/login/')
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def welcome_page(request):
     if not request.user.is_authenticated:
         return redirect("user_login")
 
     task_data = Tasks.objects.filter(user=request.user).order_by('complete_status')
+    completed_tasks = task_data.filter(complete_status=True).count();
+    incomplete_tasks = task_data.count() - completed_tasks
+    print(completed_tasks)
     context = {
         'tasks' : task_data,
+        'complete__tasks' : completed_tasks,
+        'incomplete_tasks' : incomplete_tasks
     }
     return render(request, 'task_list.html', context)
 
@@ -27,6 +36,7 @@ def mark_task(request, id):
     task.complete_status = not task.complete_status
     task.save()
     return redirect('home_page')
+
 
 def add_task(request):
 
@@ -47,6 +57,16 @@ def add_task(request):
     return render(request, 'add_task.html', {'task_form':form})
 
 
+def search_task(request):
+
+    if request.user.is_authenticated:
+        task_title = request.GET['searched_title']
+        result = Tasks.objects.filter(user=request.user).filter(title__icontains=task_title);
+
+    return render(request, "task_list.html", {'tasks':result})
+
+
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def user_signup(request):
 
     if request.method == 'POST':
@@ -61,7 +81,7 @@ def user_signup(request):
             login(request, user)
             return redirect('/')
         else:
-            return render(request,'signup.html',{'form':form, 'form_error':form.errors.as_text(),})
+            return render(request,'signup.html',{'form':form, 'form_error':form.errors.as_text()})
 
     else:
         form = SignupForm()
@@ -69,41 +89,13 @@ def user_signup(request):
     return render(request,'signup.html',{'form':form})
 
 
-# def user_login(request):
-
-#     if request.method == 'POST':
-
-#         form = AuthenticationForm(request.POST)
-#         print(form.data)
-#         # username = form.cleaned_data.get('username')
-#         # password = form.cleaned_data.get('password')
-
-#         # print(username,"ps is ",password)
-#         if form.is_valid():
-#             username = form.cleaned_data.get('username')
-#             password = form.cleaned_data.get('password')
-
-#             print(username,"ps is ",password)
-
-#             user = authenticate(request, username=username, password=password)
-
-#             print("got user",user)
-#             if user is not None:
-#                 login(request, user=user)
-#                 print("all okay")
-#                 return redirect('home_page')
-#             else:
-#                 print("form valid but err ",form.errors.as_text())
-#                 return render(request,'login.html',{'form':form, 'message':form.errors.as_text()})
-#         else:
-            
-#             return render(request,'login.html',{'form':form, 'message':"Please enter a correct username and password. Note that both fields may be case-sensitive."})
-    
-#     form = AuthenticationForm(request.POST)
-#     return render(request,'login.html',{'form':form})
-
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def user_login(request):
 
+    if request.user.is_authenticated:
+        return redirect("home_page")
+
+    form = AuthenticationForm(request.POST or None)
     if request.method == 'POST':
         uname = request.POST['username']
         upass = request.POST['password']
@@ -113,13 +105,15 @@ def user_login(request):
             login(request, user=user)
             return redirect('home_page')
         else:
+            err_msg = form.error_messages.get('invalid_login')
             form = AuthenticationForm(request.POST)
-            return render(request,'login.html',{'form':form})
+            return render(request,'login.html',{'form':form, 'form_error': err_msg+" "})
     
     form = AuthenticationForm(request.POST)
-    return render(request,'login.html',{'form':form})
+    return render(request,'login.html',{'form':form , 'form_error':''})
 
 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def user_logout(request):
     logout(request)
     return redirect('user_login')
